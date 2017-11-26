73611f3ac4be600d59728275fb94f3b55114f7f6
MdBee <matt_bearson@hotmail.com>
2017-11-26 08:57:54 -0800

    added Xcode template app files

---
+++ WeatherChallenge/WeatherChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherChallenge
+//
+//  Created by Bearson, Matt D. on 11/26/17.
+//  Copyright © 2017 Bearson, Matt D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
