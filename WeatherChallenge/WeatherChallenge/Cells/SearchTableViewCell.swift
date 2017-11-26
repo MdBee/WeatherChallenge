3f8868d91935892ff4f9220ce71799fe1418c3f5
MdBee <matt_bearson@hotmail.com>
2017-11-26 11:25:02 -0800

    added basic UI

---
+++ WeatherChallenge/WeatherChallenge/Cells/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  WeatherChallenge
+//
+//  Created by Bearson, Matt D. on 11/26/17.
+//  Copyright © 2017 Bearson, Matt D. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
