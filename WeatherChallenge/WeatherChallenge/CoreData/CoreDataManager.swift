97c11857f4b199ac82ae28017b49f3b1ecabf3bb
MdBee <matt_bearson@hotmail.com>
2017-11-26 09:14:07 -0800

    added file structure

---
+++ WeatherChallenge/WeatherChallenge/CoreData/CoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataManager.swift
+//  WeatherChallenge
+//
+//  Created by Bearson, Matt D. on 11/26/17.
+//  Copyright © 2017 Bearson, Matt D. All rights reserved.
+//
+
+import Foundation
